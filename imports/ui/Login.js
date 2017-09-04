e53ed0de89de8dc903aabe6ab52b2aa36dd8d4da
Ben Slabbert <ben@hex.co.za>
2017-09-04 22:29:42 +0200

    adding login adding redirects if user is not authenticated

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
 export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+
+        Meteor.loginWithPassword({ email }, password, (err) => {
+            console.log('login callback: ', err);
+        });
+
+    };
 
     render() {
         return (
             <div>
-                <h1>Login to Short Link</h1>
+                <h1>Short Lnk</h1>
 
-                login form here
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-            <Link to="/signup">sign up here</Link>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" ref="email" name="email" placeholder="Email" />
+                    <input type="password" ref="password" name="password" placeholder="Password" />
+                    <button>Login</button>
+                </form>
+
+                <Link to="/signup">sign up here</Link>
             </div>
         );
     };
