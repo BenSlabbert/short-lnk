697553766cc5dd99f8c589093d6c8df506307f5b
Ben Slabbert <ben@hex.co.za>
2017-09-04 21:57:40 +0200

    adding authentication

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Accounts } from 'meteor/accounts-base';
 
 export default class SignUp extends React.Component {
 
@@ -12,10 +13,18 @@ export default class SignUp extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    // form validation
-    this.setState({
-      error: 'an error.'
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Accounts.createUser({email, password}, (err) => {
+      console.log('signup callback', err);
     });
+
+    // form validation
+    // this.setState({
+    //   error: 'an error.'
+    // });
   };
 
   render() {
@@ -26,8 +35,8 @@ export default class SignUp extends React.Component {
         {this.state.error ? <p>{this.state.error}</p> : undefined}
 
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input type="password" ref="password" name="password" placeholder="Password" />
           <button>Create Account</button>
         </form>
         <Link to="/">
