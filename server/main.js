8100dceeadea060a7c867036b446e5a4c6458d01
Ben Slabbert <ben@hex.co.za>
2017-09-05 22:04:16 +0200

    adding validation class

--- server/main.js
+++ server/main.js
@@ -1,5 +1,29 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const employeeSchema = new SimpleSchema({
+    name: {
+      type: String,
+      min: 1,
+      max: 200
+    },
+    hourlyWage: {
+      type: Number,
+      min: 1
+    },
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    }
+  });
+
+  employeeSchema.validate({
+    name: 'b',
+    hourlyWage: 1,
+    email: 'ben@hex.co.za'
+  });
+
 });
