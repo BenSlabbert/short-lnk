94e4f45b7726fe6f543f4b21c91f881c9ecefcbe
Ben Slabbert <ben@hex.co.za>
2017-09-05 22:24:49 +0200

    adding server and client side validation

--- server/main.js
+++ server/main.js
@@ -1,29 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
   // code to run on server at startup
 
-  const employeeSchema = new SimpleSchema({
-    name: {
-      type: String,
-      min: 1,
-      max: 200
-    },
-    hourlyWage: {
-      type: Number,
-      min: 1
-    },
-    email: {
-      type: String,
-      regEx: SimpleSchema.RegEx.Email
-    }
-  });
+  Accounts.validateNewUser((user) => {
+    const email = user.emails[0].address;
 
-  employeeSchema.validate({
-    name: 'b',
-    hourlyWage: 1,
-    email: 'ben@hex.co.za'
+    try {
+      new SimpleSchema({
+        email: {
+          type: String,
+          regEx: SimpleSchema.RegEx.Email
+        }
+      }).validate({ email });
+    } catch (e) {
+      throw new Meteor.Error(400, e.message);
+    }
+    return true;
   });
 
 });
