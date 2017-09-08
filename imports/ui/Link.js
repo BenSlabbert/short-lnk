822ae9bf71f7dc12875a24838ebc6dda71a3485b
Ben Slabbert <ben@hex.co.za>
2017-09-08 09:09:23 +0200

    adding link api

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 
+import { Links } from '../api/links';
+
 export default class Link extends React.Component {
 
     doLogout() {
         Accounts.logout();
+    }
+
+    onSubmit(e) {
+        const url = this.refs.url.value.trim();
+
+        e.preventDefault();
+
+        if (url) {
+            Links.insert({ url });
+            this.refs.url.value = '';
+        }
     }
 
     render() {
@@ -12,6 +25,12 @@ export default class Link extends React.Component {
             <div>
                 <h1>your links</h1>
                 <button onClick={this.doLogout.bind(this)}>Log out</button>
+
+                <p>Add a link:</p>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="text" ref="url" placeholder="URL" />
+                    <button>Add links</button>
+                </form>
             </div>
         );
     };
