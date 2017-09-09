1f09ba241d88931fd0446329dfa87030b9eb53fb
Ben Slabbert <ben@hex.co.za>
2017-09-09 11:28:32 +0200

    adding meteor methdos

--- imports/api/links.js
+++ imports/api/links.js
@@ -6,6 +6,19 @@ export const Links = new Mongo.Collection('links');
 if (Meteor.isServer) {
     Meteor.publish('links', function () {
         // this.userId
-        return Links.find({userId: this.userId});
+        return Links.find({ userId: this.userId });
     });
 }
+
+Meteor.methods({
+    'links.insert'(url) {
+        if (!this.userId) {
+            throw new Meteor.Error('not authorised');
+        }
+
+        Links.insert({
+            url,
+            userId: this.userId
+        });
+    }
+});
