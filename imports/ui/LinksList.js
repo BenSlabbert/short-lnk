e2c98638103f45226cb4ba514faba01d85bc6b43
Ben Slabbert <ben@hex.co.za>
2017-09-09 10:54:10 +0200

    adding subscriptions

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Links } from '../api/links';
@@ -15,6 +16,7 @@ export default class LinksList extends React.Component {
     componentDidMount() {
         console.log('component did mount links list');
         this.linksTracker = Tracker.autorun(() => {
+            Meteor.subscribe('links');
             const links = Links.find().fetch();
             this.setState({ links });
         });
