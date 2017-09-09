d542a8797c746a948346246b30f8f2983c5acc9c
Ben Slabbert <ben@hex.co.za>
2017-09-09 17:42:05 +0200

    adding clipboard library for copying

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+
+    componentDidMount() {
+        this.clipboard = new Clipboard(this.refs.copy);
+
+        this.clipboard.on('success', () => {
+            alert('it worked');
+        }).on('error', () => {
+            alert('Unable to copy. Please do it manually');
+        });
+    }
+
+    componentWillUnmont() {
+        this.clipboard.destroy();
+    }
 
     render() {
         return (
             <div>
-                <p>url: {this.props.url}</p>
-                <p>short url: {this.props.shortUrl}</p>
+                <p>{this.props.url}</p>
+                <p>{this.props.shortUrl}</p>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl} >Copy</button>
             </div>
         );
     }
