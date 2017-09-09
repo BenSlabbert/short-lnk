9e0f5f7b5d772279bc47a025342baec8ade1b787
Ben Slabbert <ben@hex.co.za>
2017-09-09 14:17:04 +0200

    adding link redirect

--- server/main.js
+++ server/main.js
@@ -1,11 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
 import '../imports/startup/simpl-schema-config.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
 
+  WebApp.connectHandlers.use((req, res, next) => {
 
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id });
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader('location', link.url);
+      res.end();
+    } else {
+      next();
+    }
+  });
 });
