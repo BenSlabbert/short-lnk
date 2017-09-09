d050312d3dac312694e5f4e4276a40b189b2fed4
Ben Slabbert <ben@hex.co.za>
2017-09-09 21:33:40 +0200

    making the check box filter controlled input

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
 
-export default () => {
-    return (
-        <div>
-            <label>
-                <input type="checkbox" onChange={ (e) => {
-                    const isChecked = e.target.checked;
-                    Session.set('showVisible', !isChecked);
-                }}/>
-                show hidden links
-            </label>
-        </div>
-    );
+export default class LinkListFilters extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showVisible: true
+        }
+    }
+
+    componentDidMount() {
+        this.tracker = Tracker.autorun(() => {
+            this.setState({
+                showVisible: Session.get('showVisible')
+            });
+        })
+    }
+
+    componentWillUnmount() {
+        this.tracker.stop();
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={!this.state.showVisible}
+                        onChange={(e) => {
+                            const isChecked = e.target.checked;
+                            Session.set('showVisible', !isChecked);
+                        }} />
+                    show hidden links
+                </label>
+            </div>
+        );
+    }
 }
