be3a89799f39c9bf9957285f2ecd158fa71c8b17
Ben Slabbert <ben@hex.co.za>
2017-09-09 19:58:20 +0200

    adding state to buttons using settimeout for 1 sec delay

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -4,11 +4,19 @@ import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCopied: false
+        };
+    }
+
     componentDidMount() {
         this.clipboard = new Clipboard(this.refs.copy);
 
         this.clipboard.on('success', () => {
-            alert('it worked');
+            this.setState({ isCopied: true })
+            setTimeout(() => {this.setState({ isCopied: false })}, 1000);
         }).on('error', () => {
             alert('Unable to copy. Please do it manually');
         });
@@ -18,12 +26,16 @@ export default class LinksListItem extends React.Component {
         this.clipboard.destroy();
     }
 
+    getText() {
+        return this.state.isCopied ? 'Copied' : 'Copy';
+    }
+
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
-                <button ref="copy" data-clipboard-text={this.props.shortUrl} >Copy</button>
+                <button ref="copy" data-clipboard-text={this.props.shortUrl} >{this.getText()}</button>
             </div>
         );
     }
