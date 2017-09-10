d11d9a3c4aa000eaac9ac733ae3d865173acde1c
Ben Slabbert <ben@hex.co.za>
2017-09-10 12:02:48 +0200

    adding titlebar styles

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -4,9 +4,11 @@ import { Accounts } from 'meteor/accounts-base';
 
 const PrivateHeader = (props) => {
     return (
-        <div>
-            <h1>{props.title}</h1>
-            <button onClick={() => { Accounts.logout() }}>Log out</button>
+        <div className="header">
+            <div className="header__content">
+                <h1>{props.title}</h1>
+                <button className="button button--link-text" onClick={() => { Accounts.logout() }}>Log out</button>
+            </div>
         </div>
     );
 };
