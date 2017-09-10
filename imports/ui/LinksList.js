187c85477972f1c1428d0551059be0e29c6f5dc0
Ben Slabbert <ben@hex.co.za>
2017-09-10 13:53:28 +0200

    styling links

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -32,6 +32,15 @@ export default class LinksList extends React.Component {
     }
 
     renderLinksListItems() {
+
+        if (this.state.links.length == 0){
+            return (
+                <div className="item">
+                    <p className="item__status--message">No links found</p>
+                </div>
+            );
+        }
+
         return this.state.links.map((link) => {
             const shortUrl = Meteor.absoluteUrl(link._id);
             return <LinksListItem key={link._id} shortUrl={shortUrl} {...link} />
@@ -41,7 +50,6 @@ export default class LinksList extends React.Component {
     render() {
         return (
             <div>
-                <p>Links List</p>
                 <div>
                     {this.renderLinksListItems()}
                 </div>
